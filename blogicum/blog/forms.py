4238e63883adb592c67b3251f6b395d064f21bc3
Creeprus <leva.kornienko@yandex.ru>
2024-12-07 15:54:02 +0300

    I WANT TO DIE

---
+++ blogicum/blog/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+
+from blog.models import Comment, Post
+
+
+class PostForm(forms.ModelForm):
+
+    class Meta:
+        model = Post
+        exclude = ('author',)
+        widgets = {
+            'pub_date': forms.DateTimeInput(
+                format='%d/%m/%Y %H:%M',
+                attrs={'type': 'datetime-local'}
+            )
+        }
+
+
+class CommentForm(forms.ModelForm):
+
+    class Meta:
+        model = Comment
+        fields = ('text',)
